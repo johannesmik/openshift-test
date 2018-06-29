e95dba4e63d00eab101f178824f754579465f041
johannes <johannesmik@users.noreply.github.com>
2018-06-29 17:28:01 +0200

    add healthcheck

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Resource, Api
 
-@app.route('/')
-def hello():
-    """Renders a sample page."""
+application = Flask(__name__)
+api = Api(application)
+
+@application.route('/')
+def index():
     return "Hello World!"
+
+class HealthCheck(Resource):
+    def get(self):
+        return 'OK'
+
+api.add_resource(HealthCheck, '/ws/healthz/')
